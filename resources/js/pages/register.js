f61ef4d74f3ccda51c157542d4bb8b8904163cae
nythonore <niyitegekah@gmail.com>
2021-09-07 16:41:12 +0200

    add slide image

--- resources/js/pages/register.js
+++ resources/js/pages/register.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $(".login_backimg").backstretch([
-        "/images/image-1.png",
-         "/images/image-2.jpeg",
-         "/images/image-3.png"
-    ], {duration: 3000, fade: 750});
+        "/slide/image-1.png",
+        "/slide/image-2.jpeg",
+        "/slide/image-3.png"
+    ], { duration: 3000, fade: 750 });
 })
 
